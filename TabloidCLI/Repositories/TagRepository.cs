31e0c3a34407fd9b74280fb8b961d22b4151e9b0
Fiona Wee <fwee@oswego.edu>
2024-08-14 10:56:59 -0400

    work on insert tag

--- TabloidCLI/Repositories/TagRepository.cs
+++ TabloidCLI/Repositories/TagRepository.cs
@@ -83,25 +83,38 @@ namespace TabloidCLI
                 {
 
                     // Check if the tag already exists
-                    //if (TagExists(tag.Name))
-                    //{
-                    //    throw new InvalidOperationException("A tag with this name already exists.");
-                    //}
+                    //WHERE Name = @Name; 
 
+                    cmd.CommandText = @"
+                    IF EXISTS (
+                    SELECT 1
+                    FROM Tag
+                    WHERE Name = @Name; 
+                    )
 
+                    BEGIN
+                        THROW 50000, 'Tag already exists';
+                    END
+                    ELSE
+                    BEGIN
+                        INSERT INTO Tag (Name)
+                        OUTPUT INSERTED.Id
+                        VALUES(@Name)
+                    END";
+                    cmd.Parameters.AddWithValue("@Name", tag.Name);
 
-
-                    //Inserts a new row called Name into the Tag table with the @Name parameter's value.
-
-                    cmd.CommandText = @"INSERT INTO Tag (Name)
-                                                    VALUES (@Name)";
-                    cmd.Parameters.AddWithValue("@Name",tag.Name);
-             
-
-                    cmd.ExecuteNonQuery();
+                    int id = (int)cmd.ExecuteScalar();
+                    tag.Id = id; 
                 }
             }
         }
+
+
+
+
+ 
+
+
 
         public void Update(Tag tag)
         {
@@ -124,52 +137,67 @@ namespace TabloidCLI
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    //IF EXISTS..SELECT 1..UNION to check if the tag is in use in any related tables
+                    // SELECT 1 checks eixstence, if any rows match the condition, dont care about actual data
+                    //THROW >=50000 for custom error messages. 
+                    cmd.CommandText = @"
+                IF EXISTS (
+                SELECT 1 FROM AuthorTag WHERE TagId = @id 
+                UNION
+                SELECT 1 PostTag WHERE TagId = @id 
+                UNION
+                SELECT 1 BlogTag WHERE TagId = @id) 
+
+                BEGIN
+                    THROW 50000, 'Cannot delete tag because it is in use';
+                END
+                ELSE
+                BEGIN
+                    DELETE FROM Tag WHERE Id = @id;
+                END";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
 
 
 
-
-
-        public SearchResults<Tag> SearchTags(string tagName)
+        public SearchResults<Author> SearchAuthors(string tagName)
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    //t.Id AS TagId,
-                    //                           t.Name,
-                    //                        at.TagId AS AuthorTagId,
-                    //                        at.AuthorId AS AuthorId,
-                    //                        pt.TagId AS PostTagId,
-                    //                        pt.PostId AS PostId,
-                    //                        bt.TagId AS BlogTagId
-                    //                        bt.BlogId AS BlogId,
-
-                    //                     FROM Tag t 
-                    //                     LEFT JOIN AuthorTag at ON t.Id = at.TagId
-                    //                     LEFT JOIN PostTag pt ON t.Id = pt.TagId
-                    //                     LEFT JOIN BlogTag bt ON t.Id = bt.TagId
-                    //                     WHERE a.id = @id";
-                    cmd.CommandText = @"SELECT Id, Name
-                                    FROM Tag
-                                    WHERE Name LIKE @name";
-                    
+                    cmd.CommandText = @"SELECT a.id,
+                                               a.FirstName,
+                                               a.LastName,
+                                               a.Bio
+                                          FROM Author a
+                                               LEFT JOIN AuthorTag at on a.Id = at.AuthorId
+                                               LEFT JOIN Tag t on t.Id = at.TagId
+                                         WHERE t.Name LIKE @name";
                     cmd.Parameters.AddWithValue("@name", $"%{tagName}%");
                     SqlDataReader reader = cmd.ExecuteReader();
 
-                    SearchResults<Tag> results = new SearchResults<Tag>();
+                    SearchResults<Author> results = new SearchResults<Author>();
                     while (reader.Read())
                     {
-                        Tag tag = new Tag()
+                        Author author = new Author()
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Name = reader.GetString(reader.GetOrdinal("Name")),
-                            
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            Bio = reader.GetString(reader.GetOrdinal("Bio")),
                         };
-                        results.Add(tag);
+                        results.Add(author);
                     }
 
                     reader.Close();
@@ -178,5 +206,52 @@ namespace TabloidCLI
                 }
             }
         }
+
+        //public SearchResults<Tag> SearchTags(string tagName)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            //t.Id AS TagId,
+        //            //                           t.Name,
+        //            //                        at.TagId AS AuthorTagId,
+        //            //                        at.AuthorId AS AuthorId,
+        //            //                        pt.TagId AS PostTagId,
+        //            //                        pt.PostId AS PostId,
+        //            //                        bt.TagId AS BlogTagId
+        //            //                        bt.BlogId AS BlogId,
+
+        //            //                     FROM Tag t 
+        //            //                     LEFT JOIN AuthorTag at ON t.Id = at.TagId
+        //            //                     LEFT JOIN PostTag pt ON t.Id = pt.TagId
+        //            //                     LEFT JOIN BlogTag bt ON t.Id = bt.TagId
+        //            //                     WHERE a.id = @id";
+        //            cmd.CommandText = @"SELECT Id, Name
+        //                            FROM Tag
+        //                            WHERE Name LIKE @name";
+
+        //            cmd.Parameters.AddWithValue("@name", $"%{tagName}%");
+        //            SqlDataReader reader = cmd.ExecuteReader();
+
+        //            SearchResults<Tag> results = new SearchResults<Tag>();
+        //            while (reader.Read())
+        //            {
+        //                Tag tag = new Tag()
+        //                {
+        //                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                    Name = reader.GetString(reader.GetOrdinal("Name")),
+
+        //                };
+        //                results.Add(tag);
+        //            }
+
+        //            reader.Close();
+
+        //            return results;
+        //        }
+        //    }
+        //}
     }
 }
